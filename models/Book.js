933a5616cbaf0ebed471e8030e23ad2b02856fef
Jason Shory <jshory@myseneca.ca>
2019-09-12 11:04:07 -0400

    updated Member.js and Book.js

--- models/Book.js
+++ models/Book.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 const BookSchema = Schema({
-    title: { type: String, unique: true },
+    title: String,
     year: Number,
     genre: String,
     authorName: String,
--- models/Member.js
+++ models/Member.js
@@ -3,7 +3,7 @@ var Schema = mongoose.Schema;
 
 const MemberSchema = Schema({
     name: { type: String, unique: true },
-    bookList: [{ type: String, unique: true }]
+    bookList: [{ type: String }]
 });
 
 module.exports = mongoose.model('Member', MemberSchema);
